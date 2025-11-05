2eda6b017f595e6bc7fba0965510f2d5bb344c43
Saurav Anand <140311806+isauravanand@users.noreply.github.com>
2025-11-05 12:38:08 +0530

    Delete src/messageSchema.ts

--- src/messageSchema.ts
+++
@@ -1,8 +0,0 @@
-import { z } from "zod";
-
-export const messageSchema = z.object({
-    content: z
-        .string()
-        .min(10, { message: "Message Must be atleast 10 character long" })
-        .max(300, { message: "Message Must be under 300 character" })
-})
